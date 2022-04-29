6764f9b556e1ef2ebbe40574390e60e9e8424342
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-29 12:37:04 -0600

    choice 3 prep

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def adventure():
                             adventure()
                         else:
                             print("End Game")
-                    elif food == "N":
+                    else:
                         end = input(
                             "WOWZA, the " + meal + "killed you. Would you like to restart? Y/N")
                         if end == "Y":
@@ -108,10 +108,136 @@ def adventure():
                                 adventure()
                             else:
                                 print("End Game")
+                        else:
+                            end = input(
+                                "OO you're still a spicy one, but I can't take anyone on this adventure who doesn't like a little sugar, do you want to restart? Y/N")
+                            if end == "Y":
+                                adventure()
+                            else:
+                                print("End Game")
+            else:
+                no_food = input(
+                    "Ya crazy! I'll cook you something up really quick adventurer. What would you like? ")
+                if (len(no_food) < 5):
+                    easy = input(
+                        no_food + " will need a little kick, add some spice? Y/N")
+                    if easy == "Y":
+                        end = input(
+                            "OO you're a spicy one, but I put in too much spice and you've died, do you want to restart? Y/N")
+                        if end == "Y":
+                            adventure()
+                        else:
+                            print("End Game")
+                    else:
+                        end = input(
+                            "OO you're not a spicy one, but I put in too much spice anyway and you've died, do you want to restart? Y/N")
+                        if end == "Y":
+                            adventure()
+                        else:
+                            print("End Game")
+                elif (len(no_food) >= 5 and len(no_food) < 8):
+                    easy = input(
+                        no_food + " sounds yummy, should we fry it too? Y/N")
+                    if easy == "Y":
+                        end = input(
+                            "oof a little heartburn, and you're dead, do you want to restart? Y/N")
+                        if end == "Y":
+                            adventure()
+                        else:
+                            print("End Game")
+                    else:
+                        end = input(
+                            "Smart human, but without fried food you cannot truly live! So the chef has kicked you out, do you want to restart? Y/N")
+                        if end == "Y":
+                            adventure()
+                        else:
+                            print("End Game")
+                else:
+                    end = input(
+                        "Smart human, but without fried food you cannot truly live! So the chef has kicked you out, do you want to restart? Y/N")
+                    if end == "Y":
+                        adventure()
+                    else:
+                        print("End Game")
 
         else:
             choice3 = input("Spicy choice! " + location +
                             "has great squirrels. What activity do you like to do there?")
+            if choice3 == "Y":
+                meal = input(
+                    "Wonderful! It's around time for dinner, what are you cooking?")
+                if (len(meal) < 5):
+                    food = input("EWWW, why would you cook " +
+                                 meal + "Do you regret that decision? Y/N")
+                    if food == "Y":
+                        end = input(
+                            "TOO BAD, the " + meal + "killed you. Would you like to restart? Y/N")
+                        if end == "Y":
+                            adventure()
+                        else:
+                            print("End Game")
+                    else:
+                        end = input(
+                            "WOWZA, the " + meal + "killed you. Would you like to restart? Y/N")
+                        if end == "Y":
+                            adventure()
+                        else:
+                            print("End Game")
+                elif (len(meal) > 5):
+                    food = input("EWWW, why would you cook " + meal)
+                    end = input(
+                        "You died from that horrible " + meal + " would you like to restart? Y/N")
+                    if end == "Y":
+                        adventure()
+                    else:
+                        print("End Game")
+            else:
+                new_place = input(
+                    "Bummer! What kind of lodging do you prefer? ex: hotel, lodge, hostel")
+                if (len(new_place) < 4):
+                    lodging = input(
+                        new_place + "is a great choice! How long will you be staying?")
+                    if (int(lodging) < 10):
+                        end = input(
+                            lodging + " is such a short amount of time would you like to restart? Y/N")
+                        if end == "Y":
+                            adventure()
+                        else:
+                            print("End Game")
+                    else:
+                        end = input(
+                            lodging + " is such a weird amount of time would you like to restart? Y/N")
+                        if end == "Y":
+                            adventure()
+                        else:
+                            print("End Game")
+                elif (len(new_place) >= 4 and len(new_place) < 8):
+                    lodging_two = input(
+                        new_place + "is a great choice!  How long will you be staying?")
+                    print("Wow"+lodging_two+" is a long time, have a great stay!")
+                    end = input(
+                        "This is the end of our adventure, would you like to restart? Y/N")
+                    if end == "Y":
+                        adventure()
+                    else:
+                        print("End Game")
+                else:
+                    lodging_three = input(
+                        new_place + " is a great choice! How long will you be staying?")
+                    if (int(lodging_three) < 6):
+                        end = input(
+                            "What the heck, the place has termites, do you want to leave? Y/N")
+                        if end == "Y":
+                            adventure()
+                        else:
+                            print("End Game")
+                    else:
+                        end = input(
+                            "What the heck, bed bugs much? I guess it's time to end the adventure, do you want to restart? Y/N")
+                        if end == "Y":
+                            adventure()
+                        else:
+                            print("End Game")
 
     else:
         print("Whoops")
