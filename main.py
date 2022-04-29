75e78215daa487fc678837ed3bae08b03f885be1
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-29 10:06:03 -0600

    choices - start game update

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 # Create your own adventure with python here
-def start():
-    start_game = input("Would you like to start the adventure? Y/N")
-    if start_game == "Y":
-        adventure()
-    else:
-        print("Whoops")
-        start()
-
 
 def adventure():
-    name = input("Welcome Adventurer, What is your name?")
-    location = input("Welcome " + name + " Where would you like to travel?")
+    start_game = input("Would you like to start the adventure? Y/N  ")
+    if start_game == "Y":
+        name = input("Welcome Adventurer, What is your name?")
+        location = input("Welcome " + name +
+                         " Where would you like to travel?")
+        if (len(location) < 5):
+            choice1 = input("nice choice! " + location +
+                            "has great weather. Have you tried their hotdogs? Y/N")
+        elif (len(location) >= 5 and len(location) < 10):
+
+        else:
+
+    else:
+        print("Whoops")
+        adventure()
+
+
+adventure()
