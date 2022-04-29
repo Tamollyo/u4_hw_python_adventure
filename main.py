c5eed73c45c940cd2f2485b092b96eaf3a8d81a0
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-29 11:25:18 -0600

    restart functionality

--- main.py
+++ main.py
@@ -7,11 +7,37 @@ def adventure():
         location = input("Welcome " + name +
                          " Where would you like to travel?")
         if (len(location) < 5):
-            choice1 = input("nice choice! " + location +
-                            "has great weather. Have you tried their hotdogs? Y/N")
-        elif (len(location) >= 5 and len(location) < 10):
+            choice1 = input("Dapple choice! " + location +
+                            " has great weather. Do you have lodging there? Y/N")
+            if choice1 == "Y":
+                meal = input(
+                    "Wonderful! It's around time for dinner, what are you cooking?")
+            else:
+                new_meal = input(
+                    "Bummer! What kind of lodging do you prefer? ex: hotel, lodge, hostel")
+                if (len(new_meal) < 4):
+                    lodging = input(
+                        new_meal + "is a great choice! How long will you be staying?")
+                elif (len(new_meal) >= 4 and len(new_meal) < 8):
+                    lodging_two = input(
+                        new_meal + "is a great choice!  How long will you be staying?")
+                    print("Wow"+lodging_two+" is a long time, have a great stay!")
+                    end = input(
+                        "This is the end of our adventure, would you like to restart? Y/N")
+                    if end == "Y":
+                        adventure()
+                    else:
+                        print("End Game")
+                else:
+                    lodging_three = input(
+                        new_meal + " is a great choice! How long will you be staying?")
 
+        elif (len(location) >= 5 and len(location) < 10):
+            choice2 = input("Wonderful choice! " + location +
+                            "has great wild grasses. Have you eaten yet today? Y/N")
         else:
+            choice2 = input("Spicy choice! " + location +
+                            "has great squirrels. What activity do you like to do there?")
 
     else:
         print("Whoops")
