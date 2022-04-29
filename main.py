2d63eae3b0f45804fc8dc7ca7656b9c2de7e08fd
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-29 09:50:45 -0600

    start

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # Create your own adventure with python here
+def start():
+    start_game = input("Would you like to start the adventure? Y/N")
+    if start_game == "Y":
+        adventure()
+    else:
+        print("Whoops")
+        start()
+
+
+def adventure():
+    name = input("Welcome Adventurer, What is your name?")
+    location = input("Welcome " + name + " Where would you like to travel?")
